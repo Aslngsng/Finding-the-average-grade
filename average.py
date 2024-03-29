e59909e784936bb4fb70bcfe608e1629f9f09edb
Aslngsng <salangsang.abril@gmail.com>
2024-03-29 22:49:52 +0800

    getting the average grade

--- average.py
+++ average.py
@@ -1,7 +1,11 @@
 #ask the user to input their grades in Math, Science, and English
-#get the average grade of three subjects
+
 #identify if they passed or not
 
 math = float(input("Please inpur your grade in Math: "))
 science = float(input("Please input your grade in Science: "))
 english = float(input("Please input your grade in English: "))
+
+#get the average grade of three subjects
+average = ((math + science + english) / 3)
+print(int(average))
